719d83b49484e1bf67e8ed08fcadd45a59dce91e
Beatriz Sopeña Merino <beatrizsmerino@gmail.com>
2021-07-01 00:48:36 +0200

    STYLES. Updated styles
    - Added background color of gradient night
    - Added font 'Permanent Marker' and 'Oxygen Mono'
    - Changed px to rem
    - changed button to circle buttons
    - Added icon svg with sprites

--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
+/*
+ * COMPONENTS
+ * sprites
+ * =================================================
+ */
+
+/**
+ * @function printSprites
+ * @description Insert file sprites.svg with the icons to the end of the all html files
+ */
+function printSprites() {
+	const url = 'images/icons/sprites.svg';
+	const className = 'sprite';
+
+	const getContentFile = async (urlFile) => {
+		const getData = await fetch(urlFile);
+		const data = await getData.text();
+
+		return data;
+	};
+
+	getContentFile(url).then((data) => {
+		const contentSprites = document.createElement('div');
+		contentSprites.setAttribute('class', className);
+		document.querySelector('body').appendChild(contentSprites);
+		contentSprites.insertAdjacentHTML('beforeend', data);
+	});
+}
+
+(function () {
+	printSprites();
+}());
+
+
+/*
+ * APP
+ * vue
+ * =================================================
+ */
 const app = new Vue({
 	el: '#app',
 	data: {
